dbcf150ce002cf67766fc956a58b6b365aba3a2e
Gabriel Alves <gabriel0alves.oficial@gmail.com>
2024-05-31 14:06:15 -0300

    Read docs is functional

---
+++ ReadDocs.py
@@ -0,0 +1,20 @@
+
+class ReadDocs:
+
+    def __init__(self):
+        self.lines: list
+    
+    def ReadDoc(self, name_doc: str):
+        with open(name_doc,'r') as f:
+            self.lines = f.readlines()
+        print(self.lines)
+    
+    def Tokenizer(self):
+        self.lines = [line.strip().split() for line in self.lines]
+
+    def DefineMatrices(self) -> list:
+        n, m = map(int, self.lines[0][0:2])
+        c = list(map(float, self.lines[1]))
+        A = [[float(self.lines[i][j]) for j in range(0,5)] for i in range(2,5)]
+        b = list(map(float, self.lines[-1]))
+        return [n, m, c, A, b]
